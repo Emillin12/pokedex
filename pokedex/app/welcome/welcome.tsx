0983f0f23257b52564e7174da036dbcf08364b61
Emillin Barbosa <luizemillin@gmail.com>
2026-03-26 10:17:13 -0300

    feat: :sparkles: Tentamos fazer um site
    Tentamos fazer um site usando o Tailwid

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -5,8 +5,48 @@ export function Welcome() {
 
   return (
     <main>
-      <h1> Oi Bryan!</h1>
-      <button className ="border-2 border-red-300 bg-red-200 hover:cursor-pointer hover:bg-amber-50"  onClick={() => console.log("oi")}>Aperte para falar oi</button>
-    </main>
+      <div className = "text-white , bg-red-600 , " >
+        <h1>Pokédex</h1>
+          <table className="bg-red-600">
+            <h1>Home</h1>
+            <h1>Pokémons</h1>
+            <h1>Tipos</h1>
+          </table>
+      </div>
+
+      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl , bg-amber-300 p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      </div>
+
+  <div>
+    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
+    <p className="text-white">Elétrico</p>
+
+     <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl , bg-green-400, p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+      </div>
+  </div>
+  <div>
+    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
+    <p className="text-white">Elétrico</p>
+
+     <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl, bg-red-400 , p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  
+  <div>
+    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
+    <p className="text-white">Elétrico</p>
+  </div>
+
+   <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl , bg-blue-400, p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+  
+  <div>
+    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
+    <p className="text-white">Elétrico</p>
+  </div>
+  </div>
+  </div>
+</div>
+       
+      
+  </main>
+
   );
 }
