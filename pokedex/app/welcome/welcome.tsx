1e93a1023c569627dcce5d4ffdbba57260d6790c
Emillin Barbosa <luizemillin@gmail.com>
2026-04-08 15:38:50 -0300

    feat: :sparkles: Coloquei o lugar dos personagens
    ajustei o lugar dos personagens , coloquei a escrita neles e ajustei o lugar do home la

--- pokedex/app/welcome/welcome.tsx
+++ pokedex/app/welcome/welcome.tsx
@@ -5,47 +5,47 @@ export function Welcome() {
 
   return (
     <main>
-      <div className = "text-white , bg-red-600 , " >
+      <div className = "text-white , bg-red-600 , flex-15 " >
         <h1>Pokédex</h1>
-          <table className="bg-red-600">
-            <h1>Home</h1>
-            <h1>Pokémons</h1>
-            <h1>Tipos</h1>
+          <tr>
+          <table className="bg-red-600 , justify-self-end" >
+            <th>Home</th>
+            <th>Pokémons</th>
+            <th>Tipos</th>
           </table>
+          </tr>
+    
       </div>
 
-      <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl , bg-amber-300 p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-      </div>
 
-  <div>
-    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
-    <p className="text-white">Elétrico</p>
+    <div className = " flex , text-black , rounded-xl , bg-yellow-200 , w-55 , justify-center-safe  ">
+        <button className=" py-20 , ">
+            <h1>Pikachu</h1>
+            <h1>Elétrico</h1>
+        </button>
+    </div>
 
-     <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl , bg-green-400, p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-      </div>
-  </div>
-  <div>
-    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
-    <p className="text-white">Elétrico</p>
+    <div className = " flex , text-black , rounded-xl , bg-green-300 , w-55 , justify-center-safe  ">
+        <button className=" py-20 , ">
+            <h1>Pikachu</h1>
+            <h1>Elétrico</h1>
+        </button>
+    </div>
 
-     <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl, bg-red-400 , p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  
-  <div>
-    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
-    <p className="text-white">Elétrico</p>
-  </div>
+    <div className = " flex , text-black , rounded-xl , bg-red-400 , w-55 , justify-center-safe  ">
+        <button className=" py-20 , ">
+            <h1>Pikachu</h1>
+            <h1>Elétrico</h1>
+        </button>
+    </div>
 
-   <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl , bg-blue-400, p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
-  
-  <div>
-    <div className="text-xl font-medium text-black dark:text-white">Pikachu</div>
-    <p className="text-white">Elétrico</p>
-  </div>
-  </div>
-  </div>
-</div>
-       
-      
+    <div className = " flex , text-black , rounded-xl , bg-blue-300 , w-55 , justify-center-safe  ">
+        <button className=" py-20 , ">
+            <h1>Pikachu</h1>
+            <h1>Elétrico</h1>
+        </button>
+    </div>
+
   </main>
 
   );
